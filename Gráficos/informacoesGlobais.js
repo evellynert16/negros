1ad9ee90d877d9a195f353e68fe8926d458296e1
evellyn <108153487+evellynert16@users.noreply.github.com>
2024-09-19 14:55:11 -0300

    fcgn

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -10,8 +10,10 @@ async function visualizarInformacoesGlobais() {
     const porcentagemConectada = ((pessoasConectadas/pessoasMundo)*100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
+    paragrafo.classList.add('graficos-container__texto-1')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasMundo} bilhões </span> de pessoas e que aproximadamente <span> ${pessoasConectadas} bilhões </span> estão conectadas em alguma rede social e passam em média <span> ${horas} horas </span> e <span>${minutos} minutos </span> conectadas?<br>Isso significa que <span>${porcentagemConectada} % </span> de pessoas estão conectadas em alguma rede social `
     const container = document.getElementById('graficos-container')
+    const container = document.getElementById('graficos-container-1')
     container.appendChild(paragrafo)
 }
 visualizarInformacoesGlobais()
