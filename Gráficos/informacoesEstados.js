2011e90c5a3ddad66f9ce5efd9cf336cbbe361f3
evellyn <108153487+evellynert16@users.noreply.github.com>
2024-09-26 15:04:27 -0300

    t

--- Gráficos/informacoesEstados.js
+++ Gráficos/informacoesEstados.js
@@ -1,16 +1,16 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'informacoes.json'
 
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas/1e9)
-    const pessoasMundo = (dados.total_pessoas_mundo/1e9)
+    const pessoasConectadas = (dados.total_pessoas_conectadas)
+    const pessoasMundo = (dados.total_pessoas_mundo)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) *100)
     const porcentagemConectada = ((pessoasConectadas/pessoasMundo)*100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasMundo} bilhões </span> de pessoas e que aproximadamente <span> ${pessoasConectadas} bilhões </span> estão conectadas em alguma rede social e passam em média <span> ${horas} horas </span> e <span>${minutos} minutos </span> conectadas?<br>Isso significa que <span>${porcentagemConectada} % </span> de pessoas estão conectadas em alguma rede social `
+    paragrafo.innerHTML = `A região Nordeste é a que concentra o maior número de localidade quilombolas no Brasil, com <span> ${pessoasConectadas} Quilombos  </span> a região Sudeste vem em seguida,com <span> ${pessoasconectadas} quilombo. </span> Em relação á população quilombola, os estados da Bahia e Maranhão concentram metade da população quilombola do Brail isso siginifica que são <span>${porcentagemConectadas} % </span> de Quilobos.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
