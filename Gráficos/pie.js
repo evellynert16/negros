ac72d52c55c9e98f39d237767d8a189f2cbdf93c
evellyn <108153487+evellynert16@users.noreply.github.com>
2024-09-26 13:37:26 -0300

    sdrg

--- Gráficos/pie.js
+++ Gráficos/pie.js
@@ -21,7 +21,7 @@ async function quantidadeUsuarios() {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
         title: {
-            text: 'Redes sociais com mais usuários no mundo',
+            text: 'Estados do Brasil',
             font: {
                 color: getCSS('--cor-principal'),
                 family: getCSS('--fonte'),
@@ -31,7 +31,7 @@ async function quantidadeUsuarios() {
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Nome das redes sociais',
+                text: 'Estados',
                 font: {
                     color: getCSS('--cor-secundaria')
                 }
