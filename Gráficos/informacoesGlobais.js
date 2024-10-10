20af1f821d77606b1eeecc921a5a27f28ae30e0c
evellyn <108153487+evellynert16@users.noreply.github.com>
2024-10-10 14:59:55 -0300

    aaa

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -10,8 +10,7 @@ async function visualizarInformacoesGlobais() {
     const porcentagemConectada = ((pessoasConectadas/pessoasMundo)*100).toFixed(2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `No primeiro gráfico podemos analisar os intrumentos musicais mais usados e utilizados no continente Áfricano
-    .`
+    paragrafo.innerHTML = `No primeiro gráfico podemos analisar os intrumentos musicais mais usados e utilizados no continente Áfricano. Já no segundo gráfico observamos o indice de racismo nos Estados do Brasil a cada 100 mil habitantes.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
